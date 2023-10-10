f4217fae989e84fa73c74794707303b921d734da
Daniel Verduras Gallego <daniel.verduras21@gmail.com>
2023-10-10 12:09:34 +0200

    Initial Commit

---
+++ src/main/java/com/uah/f1backend/F1backendApplication.java
@@ -0,0 +1,13 @@
+package com.uah.f1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uah/f1backend/F1backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uah.f1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
