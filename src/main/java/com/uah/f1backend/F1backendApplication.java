c6bd8cecf6b8e238b58f41b542b49cc8e471d113
Daniel Verduras Gallego <daniel.verduras21@gmail.com>
2023-10-16 22:34:08 +0200

    Create V1 Database

--- src/main/java/com/uah/f1backend/F1backendApplication.java
+++ src/main/java/com/uah/f1backend/F1backendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class F1backendApplication {
 
 	public static void main(String[] args) {
