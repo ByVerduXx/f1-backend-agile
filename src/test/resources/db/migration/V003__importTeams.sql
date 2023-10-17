f22c958914140b8e8befbdb3a07eb28a164738d5
K4STR0 <74238421+K4STR0@users.noreply.github.com>
2023-10-17 12:06:26 +0200

    Add db team examples

---
+++ src/test/resources/db/migration/V003__importTeams.sql
@@ -0,0 +1,4 @@
+INSERT INTO team (id, team.name, logo, twitter) VALUES
+                                                    (1, 'Scuderia Ferrari', 'ferrari.png', '@ScuderiaFerrari'),
+                                                    (2, 'McLaren Formula 1 Team', 'mclaren.png', '@McLarenF1'),
+                                                    (3, 'Oracle Red Bull Racing', 'redbull.png', '@redbullracing');
