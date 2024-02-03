59d435a8a83e1554cec9a4347da43108d3fb7682
jmanufa01 <jose.fando@edu.uah.es>
2024-02-03 17:59:55 +0100

    Update db Flyway migrations

--- src/main/resources/db/migration/V007__createNewsTable.sql
+++ src/main/resources/db/migration/V007__createNewsTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS  `f1-agile`.`news` (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    permalink VARCHAR(100) UNIQUE NOT NULL,
+    permalink VARCHAR(100) NOT NULL,
     title VARCHAR(100) NOT NULL,
-    image VARCHAR(255),
-    text TEXT CHECK(LENGTH(text) >= 500 AND LENGTH(text) <= 2000),
+    image VARCHAR(255) NOT NULL,
+    text VARCHAR(2000) NOT NULL,
     publication_date DATE)
     ENGINE = InnoDB;
